a59e28051e12aa9be500eca9463cf96292e06866
SerjoGon <mr.serhioru@mail.ru>
2022-07-11 22:12:28 +0300

    Initialaizer list

--- DataContainers/ForwardList/main.cpp
+++ DataContainers/ForwardList/main.cpp
@@ -2,15 +2,16 @@
 using std::cout;
 using std::cin;
 using std::endl;
-
+#define DELIMETER "\n-------------------------------------\n"
 #define tab "\t"
-
+class ForwardList;
 class Element
 {
 	int Data;
 	Element* pNext;
 	static int count;
 public:
+	friend ForwardList operator+(const ForwardList& left, const ForwardList& right);
 	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
 	{
 		count++;
@@ -36,15 +37,30 @@ public:
 		size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
+	ForwardList(std::initializer_list<int>il) : ForwardList()
+	{
+		cout << typeid(il.begin()).name() << endl;
+		for (int const* it = il.begin();it != il.end();it++) push_back(*it);
+	}
 	ForwardList(const ForwardList& other) :ForwardList()
 	{
-		Element* Temp = other.Head;
+		/*Element* Temp = other.Head;
 		while (Temp)
 		{
 			push_back(Temp->Data);
 			Temp = Temp->pNext;
-		}
+		}*/
+		*this = other;
 		cout << "CopyConstructor:\t" << this << endl;
+	}
+	ForwardList(ForwardList&& other)noexcept :ForwardList()
+	{
+		/*this->Head = other.Head;
+		this->size = other.size;
+		other.Head = nullptr;
+		other.size = 0;*/
+		*this = std::move(other);
+		cout << "MoveConstructor:\t" << this << endl;
 	}
 	~ForwardList()
 	{
@@ -54,23 +70,32 @@ public:
 	// operators:
 	ForwardList& operator=(const ForwardList& other)
 	{
+		if (this == &other)return *this;
 		clear();
 		Element* Temp = other.Head;
-		while(Temp)
+		for (Element* Temp = other.Head;Temp ;Temp = Temp->pNext)push_back(Temp->Data);
+		/*while(Temp)
 		{
 			push_back(Temp->Data);
 			Temp = Temp->pNext;
-		}
+		}*/
 		cout << "CopyAssigment:\t" << this << endl;
+		return *this;
+	}
+	ForwardList& operator=(ForwardList&& other)noexcept
+	{
+		this->Head = other.Head;
+		this->size = other.size;
+		other.Head = nullptr;
+		other.size = 0;
+		cout << "MoveAssignment:\t" << this << endl;
 		return *this;
 	}
 
 	//				Adding Elements:
 	void push_front(int Data)
 	{
-		Element* New = new Element(Data);
-		New->pNext = Head;
-		Head = New;
+		Head = new Element(Data,Head);
 		size++;
 	}
 	void push_back(int Data)
@@ -89,10 +114,9 @@ public:
 		//	current->pNext = new Element(Data);
 		//}
 		if (Head == nullptr)return push_front(Data);
-		Element* New = new Element(Data);
 		Element* Temp = Head;
 		while (Temp->pNext != nullptr) Temp = Temp->pNext;
-		Temp->pNext = New;
+		Temp->pNext = new Element(Data);
 		size++;
 	}
 	void insert(int Data,int index)
@@ -102,13 +126,11 @@ public:
 		if (index == size)return push_back(Data);
 		else
 		{
-			Element* previous = this->Head;
-			for (int i = 0; i < index -2; i++)
-			{
-				previous = previous->pNext;
-			}
-			Element* NewElem = new Element(Data, previous->pNext);
-			previous->pNext = NewElem;
+			Element* previous = Head;
+			for (int i = 0; i < index -1; i++) previous = previous->pNext;
+			/*Element* NewElem = new Element(Data, previous->pNext);
+			previous->pNext = NewElem;*/
+			previous->pNext = new Element(Data, previous->pNext);
 		}
 		size++;
 	}
@@ -158,18 +180,43 @@ public:
 	//				Methods:
 	void  print()const
 	{
-		Element* Temp = Head;
+		/*Element* Temp = Head;
 		while (Temp)
 		{
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext;
+		}*/
+		for (Element* Temp = Head; Temp ; Temp = Temp->pNext)
+		{
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 		}
 		cout << "Количество элементов списка: " << size << endl;
 		cout << "Общее количество элементов: " << Element::count << endl;
 	}
+	friend ForwardList operator+(const ForwardList& left, const ForwardList& right);
 };
-#define BASE_CHECK
+
+ForwardList operator+(const ForwardList& left, const ForwardList& right)
+{
+	ForwardList cat = left;
+	/*Element* Temp = right.Head;
+	while (Temp)
+	{
+		cat.push_back(Temp->Data);
+		Temp = Temp->pNext;
+	}*/
+	for (Element* Temp = right.Head;Temp ;Temp = Temp->pNext)
+	{
+		cat.push_back(Temp->Data);
+	}
+	cout << "Оператор + " << endl;
+	return cat;
+}
+
+//#define BASE_CHECK
 //#define COUNT_CHECK
+//#define OPERATOR_PLUS_CHECK
+//#define RANGE_BASED_ARRAY
 int main()
 {
 	setlocale(LC_ALL, "Ru");
@@ -187,27 +234,23 @@ int main()
 		list.push_front(rand() % 100);
 	}
 	list.print();
-	list.erase(3);
-	list.print();
-	list.push_back(123);
-	list.print();
 
 	ForwardList list_2;
+	ForwardList list_3;
 	for (int i = 0; i < n; i++)
 	{
 		list_2.push_back(rand() % 30);
 	}
+	list_2.print();
 	cout << "Введите индекс добавочного элемента! ";cin >> index;
 	cout << "Введите значение добавочного элемента! ";cin >> value;
-	list_2.print();
 	list_2.insert(value, index);
 	list_2.print();
-	list_2.clear();
+	list_3 = list + list_2;
+	list_3.print();
 
 	//ForwardList list3 = list;
-	ForwardList list3;
-	list3 = list;
-	list3.print();
+	
 #endif // BASE_CHECK
 #ifdef COUNT_CHECK
 
@@ -228,6 +271,39 @@ int main()
 	list_2.push_back(13);
 	list_2.print();
 #endif // COUNT_CHECK
+#ifdef OPERATOR_PLUS_CHECK
+	ForwardList list;
+	list.push_back(12);
+	list.push_back(13);
+	list.push_back(15);
+	list.push_back(32);
+	list.push_back(1212);
+	list.push_back(99);
+	list.print();
+	ForwardList list_2;
+	list_2.push_back(3);
+	list_2.push_back(45);
+	list_2.push_back(6);
+	list_2.push_back(4);
+	list_2.push_back(13);
+	list_2.print();
+	cout << DELIMETER;
+	//ForwardList list3 =  list + list_2;// MoveConstructor
+	ForwardList list3;
+	list3 = list + list_2;
+	cout << DELIMETER;
+	list3.print();
+#endif // OPERATOR_PLUS_CHECK
+#ifdef RANGE_BASED_ARRAY
+	int arr[] = { 3,5,8,13,21 };
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]);i++)cout << arr[i] << tab;
+	cout << endl;
+	for (int i : arr)// range-base for(цикл for на основе диапазона, цикл для контейнеров)
+		cout << i << tab;// итератор i  последовательно принимает значение каждого элемента массива.
+	cout << endl;
+#endif // RANGE_BASED_ARRAY
+	ForwardList list = { 3,5,8,13,21 };
+	list.print();
 
 	//---------------------------------
 	return 0;
