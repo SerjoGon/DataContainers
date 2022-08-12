c95432607e047a08253016222fc27dba507f7517
SerjoGon <mr.serhioru@mail.ru>
2022-08-12 21:54:47 +0300

    BinariTree Start

---
+++ DataContainers/BinaryTree/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define tab  "\t"
+#define DELIMETER "\n=================================\n"
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	return 0;
+}
