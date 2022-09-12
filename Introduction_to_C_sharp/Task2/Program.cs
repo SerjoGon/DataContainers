c3c66a4522c7fe3b9265b70ebdf92adad994fd8f
SerjoGon <mr.serhioru@mail.ru>
2022-09-12 15:00:29 +0300

    Task2

---
+++ Introduction_to_C_sharp/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число!");
+            string number = Console.ReadLine();
+            Console.WriteLine("Введите процент первого числа");
+            string percent = Console.ReadLine();
+            Console.WriteLine("{1}% от числа {0} = {2}",number,percent,Convert.ToDouble(number)*(Convert.ToDouble(percent) / 100));
+        }
+    }
+}
