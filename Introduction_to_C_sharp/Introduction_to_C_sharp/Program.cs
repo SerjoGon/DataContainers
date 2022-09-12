f31435dd684150fb883914caf87c699a7ad7c0ba
SerjoGon <mr.serhioru@mail.ru>
2022-09-12 14:39:01 +0300

    FizzBuzz!

---
+++ Introduction_to_C_sharp/Introduction_to_C_sharp/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Привет, введи число от 1 до 100!");
+string? number = Console.ReadLine();
+double operand = Convert.ToInt32(number);
+if(operand > 1 && operand <101)
+{
+    if (operand % 3 != 0 && operand % 5 != 0) Console.WriteLine(operand);
+    if (operand % 3 == 0) Console.Write("Fizz ");
+    if (operand % 5 == 0) Console.WriteLine("Buzz!");
+}
+else
+{
+    Console.WriteLine("Ошибка! Перезапустите и введите число от 1 до 100!!!");
+}
