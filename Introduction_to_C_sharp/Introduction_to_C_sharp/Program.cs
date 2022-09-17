fbb20ad2aff839396f21410c0c940e42634d9f73
SerjoGon <mr.serhioru@mail.ru>
2022-09-17 08:20:22 +0300

    error correction

--- Introduction_to_C_sharp/Introduction_to_C_sharp/Program.cs
+++ Introduction_to_C_sharp/Introduction_to_C_sharp/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Привет, введи число от 1 до 100!");
 string? number = Console.ReadLine();
 double operand = Convert.ToInt32(number);
-if(operand > 1 && operand <101)
+if(operand >= 1 && operand <101)
 {
     if (operand % 3 != 0 && operand % 5 != 0) Console.WriteLine(operand);
     if (operand % 3 == 0) Console.Write("Fizz ");
